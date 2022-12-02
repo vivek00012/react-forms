ed048d3090a3c8c0daca8b653753336e527cba3c
vivek00012 <35500124+vivek00012@users.noreply.github.com>
2022-12-02 23:39:19 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,89 @@
-import React,{useRef,useState} from "react";
-import "./style.css";
+import React, { useRef, useState, useEffect } from 'react';
+import './style.css';
 // dom manupulation -create form save and submit
-const App=() =>{
+const App = () => {
   const inputElement = useRef();
-  const [color, setColor] = useState("");
- 
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState('');
+  const [agePlaceHolder, setAgePlaceHolder] = useState('');
+
+  const [city, setCity] = useState('');
+
+  const submit = () => {
+    console.log(firstName, lastName, age, city);
+  };
+
+  useEffect(() => {
+    setAgePlaceHolder(() => {
+      if (!age) {
+        return;
+      }
+      if (age < 18) {
+        return 'You are very young, go back';
+      } else if (age > 40 && age < 100) {
+        return 'You are too old';
+      } else if (age > 100) {
+        setAge('');
+        return 'Invalid Entry';
+      }
+    });
+  }, [age]);
+
   return (
-    <>
-    <input type="text" ref={inputElement} />
-    </>
+    <form className="simpleForm content-mid">
+      <section>
+        <label>FirstName:</label>
+        <input
+          type="text"
+          ref={inputElement}
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </section>
+      <section>
+        <label>LastName:</label>
+        <input
+          type="text"
+          ref={inputElement}
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </section>
+      <section>
+        <label>Age:</label>
+        <div className="flex-column">
+          <input
+            type="text"
+            ref={inputElement}
+            value={age}
+            onChange={(e) => {
+              console.log(!isNaN(e.target.value));
+              return !isNaN(e.target.value)
+                ? setAge(e.target.value)
+                : setAge('');
+            }}
+          />
+          <span>{agePlaceHolder}</span>
+        </div>
+      </section>
+
+      <section>
+        <label>City:</label>
+        <input
+          type="text"
+          ref={inputElement}
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+      </section>
+      <div class="text-align-initial">
+        <button type="button" onClick={submit}>
+          Submit
+        </button>
+      </div>
+    </form>
   );
-}
+};
 
 export default App;
